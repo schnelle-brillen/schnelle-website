7c7664fa049cf917408bd4dd4d88675226dac8a8
PaulsBecks <paul.beck.berlin@gmail.com>
2019-07-31 23:14:33 +0200

    added placeholder page

---
+++ src/container/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Home.css';
+
+export default () => {
+    return <div className="sb-de-home-container">
+        <h1>
+            <span role="img" aria-label="An image of a fast car, like the glasses on this page!">🏎️</span>
+            <i>Super schnell, so wie deine Brille!</i>
+            <span role="img" aria-label="Cloud symbolising the speed of the text you just read!">💨</span>
+        </h1>
+    </div>;
+}
