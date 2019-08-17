e2ae09880bf06a190a134827e4724d5e776cce71
PaulsBecks <paul.beck.berlin@gmail.com>
2019-08-17 12:15:23 +0200

    added github hook

---
+++ cd_github_hook.js
@@ -0,0 +1,25 @@
+// thanks Dylan Seidt https://www.digitalocean.com/community/tutorials/how-to-use-node-js-and-github-webhooks-to-keep-remote-projects-in-sync for the tutorial
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const secret = process.env.GITHUB_HOOK_SECRET;
+const repo = '';
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] === sig) {
+            exec('cd ' + repo + ' && git pull', () => {
+                exec('cd ' + repo + ' && npm run build');
+            });
+        }
+    });
+
+    res.end();
+}).listen(8080);
