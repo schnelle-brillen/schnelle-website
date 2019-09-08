8ee35c681653d4642d9934b4912d1e30e988f86a
PaulsBecks <paul.beck.berlin@gmail.com>
2019-09-08 11:03:57 +0200

    adapted github hook

--- cd_github_hook.js
+++ cd_github_hook.js
@@ -1,25 +1,37 @@
 // thanks Dylan Seidt https://www.digitalocean.com/community/tutorials/how-to-use-node-js-and-github-webhooks-to-keep-remote-projects-in-sync for the tutorial
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
+const http = require("http");
+const crypto = require("crypto");
+const exec = require("child_process").exec;
+
 const secret = process.env.GITHUB_HOOK_SECRET;
-const repo = '~/schnelle-website';
+const repoPath = "~/schnelle-website";
+const APP_NAME = "schnelleWebsite";
 
-const http = require('http');
-const crypto = require('crypto');
-const exec = require('child_process').exec;
+http
+  .createServer(function(req, res) {
+    req.on("data", function(chunk) {
+      let sig =
+        "sha1=" +
+        crypto
+          .createHmac("sha1", secret)
+          .update(chunk.toString())
+          .digest("hex");
 
-http.createServer(function (req, res) {
-    req.on('data', function(chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
-        if (req.headers['x-hub-signature'] === sig) {
-            exec('cd ' + repo + ' && git pull', () => {
-                exec('cd ' + repo + ' && yarn install && yarn run build');
+      if (req.headers["x-hub-signature"] === sig) {
+        exec(`cd ${repoPath} && git pull`, () => {
+          exec(`cd ${repoPath} && yarn install && yarn run build`, () => {
+            exec(`pm2 stop ${APP_NAME}`, () => {
+              exec(`pm2 start npm --name ${APP_NAME} -- start`);
             });
-        }
+          });
+        });
+      }
     });
 
     res.end();
-}).listen(8080);
+  })
+  .listen(8080);
